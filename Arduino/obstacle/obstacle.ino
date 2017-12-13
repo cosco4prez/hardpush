236af53da59862fb821b766fae2edc0cbbf9afe0
cosco4prez <34481092+cosco4prez@users.noreply.github.com>
2017-12-13 12:18:10 +0100

    clean up
    clean up and comments

--- Arduino/obstacle/obstacle.ino
+++ Arduino/obstacle/obstacle.ino
@@ -1,12 +1,13 @@
 int obstacleSensor = 10; //pin for sensor
-int obstacleCount = 0;
+int obstacleCount = 0; // for differentiating between one time events and continuos obstacle
+int obstacleInterval = 1000; // interval (ms)
 
+// CABLING LAYOUT
 // GND = GND
 // V+ = 5V
 // Signal = Pin 10
 // Enable = Not needed with jumper on
 
- 
 void setup ()
 {
   Serial.begin(9600); // Init serial output
@@ -19,19 +20,17 @@ void loop(){
 void obstacle ()
 {
   bool val = digitalRead (obstacleSensor) ; // read sensor signal
-  
-  
-  if (val == HIGH){ // if sensor signal, turn on led
-    Serial.println("No obstacles");
+
+  // if sensor signal, turn on led and report obstacle
+  if (val == HIGH){
+    //Serial.println("No obstacles");
     obstacleCount = 0;
    }
   else {
-    Serial.println("Obstacle");
+    //Serial.println("Obstacle");
     obstacleCount++;
     (obstacleCount >= 10) ? (Serial.println("Something is probably full!")) : Serial.println("Something was there, better wait if it's still there after next cycle");
     (obstacleCount >= 100) ? (obstacleCount = 0):false;
   }
-  //Serial.println("------------------------------------");
-  delay(1000); // interval
-  
+  delay(obstacleInterval);
 }
