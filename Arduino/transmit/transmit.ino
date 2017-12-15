9a5d359a4f23f8bd70941b8e58a4a0adc7a416ea
cosco4prez <34481092+cosco4prez@users.noreply.github.com>
2017-12-15 12:01:13 +0100

    new

--- Arduino/transmit/transmit.ino
+++ Arduino/transmit/transmit.ino
@@ -1,36 +1,32 @@
 #include <VirtualWire.h>
 #include <obstacleSensor.h>
+
+// VARS FOR WIRE
 uint8_t counter = 0; 
 char msg[1];
-obstacleSensor obst(10,1000);
+
+//INIT SENSOR CLASS
+obstacleSensor obst(2,1000);
 
 void setup() {
-    /*
-     * Set TX pin -> Connect FS1000A here
-     */
+    //SETUP VIRTUAL WIRE
     vw_set_tx_pin(12);
-    /*
-     * Set RX pin -> Connect XY-MK-5V here
-     */
-    vw_set_rx_pin(11);
-    
-    /*
-     * Set transmission speed. Reciver and trasmitter have to use the same speed
-     */
     vw_setup(1000);
     
+    //FOR DEBUGGING
+    Serial.begin(9600); 
 }
 
 void loop() {
-
+  //IF SENSOR SEES SOMETHING, SEND 1, OTHERWISE SEND 0
   (obst.start()== 1) ? msg[0] = 1 : msg[0] = 0;
-  /*
-   * Send buffer
-   */
+  
+  //FOR DEBUGGING
+  Serial.print("Sensor returned: ");
+  Serial.println(+obst.start());
+
+  //SEND
   vw_send((uint8_t *)msg, 1);
-  /*
-   * Wait until trasmission is complete
-   */
   vw_wait_tx();
   delay(1000);
 }
