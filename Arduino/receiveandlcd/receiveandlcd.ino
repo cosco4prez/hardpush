102f4407c1be8995d2da3e63c02ec47047e5f48e
cosco4prez <34481092+cosco4prez@users.noreply.github.com>
2017-12-19 09:22:24 +0100

    recv and lcd

---
+++ Arduino/receiveandlcd/receiveandlcd.ino
@@ -0,0 +1,53 @@
+/*
+  LiquidCrystal Library - scrollDisplayLeft() and scrollDisplayRight()
+
+  The circuit:
+ * LCD RS pin to digital pin 12
+ * LCD Enable pin to digital pin 11
+ * LCD D4 pin to digital pin 5
+ * LCD D5 pin to digital pin 4
+ * LCD D6 pin to digital pin 3
+ * LCD D7 pin to digital pin 2
+ * LCD R/W pin to ground
+ * 10K resistor:
+ * ends to +5V and ground
+ * wiper to LCD VO pin (pin 3)
+*/
+
+#include <Receiver.h>
+#include <LiquidCrystal.h>
+
+// initialize the library by associating any needed LCD interface pin
+// with the arduino pin number it is connected to
+//const int rs = 8, en = 9, d4 = 4, d5 = 5, d6 = 6, d7 = 7;
+const int rs = 12, en = 10, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+String mystring = "Push Harder!";
+int strLength = mystring.length();
+
+Receiver recv(11,1000);
+
+void setup() {
+  Serial.begin(9600);
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+  // Print a message to the LCD.
+  
+  recv.setup();
+}
+
+void loop() {
+
+  int s = recv.start();
+  if(s == 1) {
+    lcd.print(mystring);
+    delay(250);
+    
+  }
+  //Serial.println(s);
+ // delay at the end of the full loop:
+  delay(1000);
+  lcd.clear();
+
+}
+
